487893b67109179f1049711e4657b8f5d795c015
vits56 <bastosdduartebruno@gmail.com>
2022-11-07 20:35:45 -0300

    pegando os inputs do formulario contato

--- validate.js
+++ validate.js
@@ -1,1 +1,4 @@
-//Seu JavaScript de validação aqui
+let inputName = document.querySelector("#nome");
+let inputEmail = document.querySelector("#email");
+let inputAssunto = document.querySelector("#assunto");
+let inputMessage = document.querySelector("#mensagem");
